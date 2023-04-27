0775428a91ebfb03371b6a755556890817da961b
PedroLuisBrilhadori <pedroluisbrilhadori@gmail.com>
2023-04-26 23:37:32 -0300

    bot muda de nome de acordo com a hora atual

---
+++ src/index.ts
@@ -0,0 +1,31 @@
+// Require the necessary discord.js classes
+import { Client, Events, GatewayIntentBits, GuildMember } from "discord.js";
+import * as dotenv from "dotenv";
+
+dotenv.config();
+const { TOKEN, GUILD } = process.env;
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+client.guilds.client.once(Events.ClientReady, async (c) => {
+  console.log(`Bot start!`);
+  const guild = await client.guilds.fetch(GUILD);
+  const bot = await guild.members.fetch(c.user.id);
+
+  setHour(bot);
+
+  setInterval(async () => {
+    setHour(bot);
+  }, 60000);
+});
+
+function setHour(bot: GuildMember) {
+  const date = new Date();
+  const hours = date.getHours();
+  const minutes = date.getMinutes();
+  const nickname = `${hours}:${minutes}`;
+
+  bot.setNickname(`Hora certa: ${nickname}`);
+}
+
+client.login(TOKEN);
